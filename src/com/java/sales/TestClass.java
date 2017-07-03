43171450af4a5839469cbb0fdd29038f291de67a
naved khan <navedbc@gmail.com>
2017-07-03 16:03:32 -0700

    initial commit

---
+++ src/com/java/sales/TestClass.java
@@ -0,0 +1,16 @@
+package com.java.sales;
+
+public class TestClass {
+	
+	static String[] names={"lol","nav","yogi","amit"};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(String str:names){
+			System.out.println(str);
+		}
+
+	}
+
+}
